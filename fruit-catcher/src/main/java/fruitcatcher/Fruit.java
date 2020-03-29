7cd754d2ef253f3430bf29cb676b1dbd6ab7e32f
IkBenDeSjaak <58164424+IkBenDeSjaak@users.noreply.github.com>
2020-03-29 11:55:14 +0200

    Starting to implement the fruit object generator

--- fruit-catcher/src/main/java/fruitcatcher/Fruit.java
+++ fruit-catcher/src/main/java/fruitcatcher/Fruit.java
@@ -1,16 +1,30 @@
 package fruitcatcher;
 
 import java.util.List;
+import java.util.Random;
 
 import nl.han.ica.oopg.objects.GameObject;
 
 public class Fruit extends FallingObject {
+	
+	private Random random;
 
 	public Fruit(FruitCatcher fruitCatcher) {
 		// Use `.concat ()` to string 2 strings together.
 		// The method returned a new String.
 		super(fruitCatcher);
+		random = new Random();
 	}
+	
+//	private String generateFruitObject() {
+//		int randomNumber;
+//		String fruit;
+//		randomNumber = random.nextInt(4);
+//		
+//		if(randomNumber == 0) {
+//			fruit = "apple.png";
+//		}
+//	}
 
 	// @Override
 	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
